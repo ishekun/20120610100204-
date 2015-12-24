c213216a8f445cad41c4cf9c18bcae8b6149a02e
ishekun <1102154025@qq.com>
2015-12-24 15:11:43 +0800

    Bluetooth

---
+++ app/src/main/java/com/socket/ChatMsgEntity.java
@@ -0,0 +1,62 @@
+package com.socket;
+
+/**
+ * Created by 何坤 on 2015/12/24.
+ */
+
+
+public class ChatMsgEntity {
+    private static final String TAG = ChatMsgEntity.class.getSimpleName();
+
+    private String name;
+
+    private String date;
+
+    private String text;
+
+    private boolean msgType = true;
+
+    public boolean getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(boolean msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+
+    public ChatMsgEntity() {
+    }
+
+    public ChatMsgEntity(String name, String date, String text, boolean msgType) {
+        this.name = name;
+        this.date = date;
+        this.text = text;
+        this.msgType = msgType;
+    }
+}
